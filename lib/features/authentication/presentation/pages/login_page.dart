710f6a21cf30722829b8f3d9739b9709933c2407
Machuca Juan <machucajuangabriel@gmail.com>
2021-10-26 23:35:17 -0300

    page login

---
+++ lib/features/authentication/presentation/pages/login_page.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Login Page"),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 50.0),
+              child: Center(
+                child: Container(
+                    width: 200,
+                    height: 150,
+                    child: Image.asset(
+                      'assets/images/flutter.png',
+                      fit: BoxFit.contain,
+                    )),
+              ),
+            ),
+            Padding(
+              //padding: const EdgeInsets.only(left:15.0,right: 15.0,top:0,bottom: 0),
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              child: TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Email',
+                    hintText: 'Enter valid email id as abc@gmail.com'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                  left: 15.0, right: 15.0, top: 15, bottom: 0),
+              //padding: EdgeInsets.symmetric(horizontal: 15),
+              child: TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Password',
+                    hintText: 'Enter secure password'),
+              ),
+            ),
+            MaterialButton(
+              onPressed: () {},
+              child: Text(
+                'Forgot Password',
+                style: TextStyle(color: Colors.blue, fontSize: 15),
+              ),
+            ),
+            Container(
+              height: 50,
+              width: 250,
+              decoration: BoxDecoration(
+                  color: Colors.blue, borderRadius: BorderRadius.circular(20)),
+              child: MaterialButton(
+                onPressed: () {},
+                child: Text(
+                  'Login',
+                  style: TextStyle(color: Colors.white, fontSize: 25),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 130,
+            ),
+            Text('New User? Create Account')
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import "package:flutter/material.dart";
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:packs_flutter/AppModule.dart';
-import 'package:packs_flutter/AppWidget.dart';
+import 'package:packs_flutter/app_module.dart';
+import 'package:packs_flutter/app_widget.dart';
 
 void main() {
   runApp(ModularApp(module: AppModule(), child: AppWidget()));
